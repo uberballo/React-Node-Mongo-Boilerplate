a3cd9ad7d8605a36e3d26b3e1ec06b461e0d8fe3
Kasper Paavolainen <kasper.paavolainen@orangit.fi>
2020-10-30 16:14:04 +0200

    Update the start script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -f ./backend/.env ]; then
-    echo "env file missing"
+    echo "env file missing. Please run 'cp backend/.env.example backend/.env'"
     exit 1
 fi
 
-docker-compose up
+#docker-compose up
